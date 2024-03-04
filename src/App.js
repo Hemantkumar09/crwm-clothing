1b94997f9d39c61bc1bbea4f96578e449b843edf
Hemant Kumar <hemant9451.k@gmail.com>
2024-03-04 20:23:48 +0530

    we have commited two components categories-menu and category-item

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './categories.style.scss';
+//import CategoryItem from './component/category-item/category-item.component.jsx';
+import CategoriesMenu from './component/categories-menu/categories-menu.component.jsx';
+
+
+const App = () => { 
+  
+  return (
+    <CategoriesMenu />
+  );
+}
+
+export default App;
